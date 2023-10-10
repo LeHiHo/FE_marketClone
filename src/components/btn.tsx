fd2943f4a516c5547ea4df600a6c20ad28a3acf5
sue <sue@sueui-MacBookAir.local>
2023-10-10 23:16:57 +0900

    Feat: Add Button Props

--- src/components/btn.tsx
+++ src/components/btn.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import '@/styles/components/btn.scss';
 import Link from 'next/link';
 
-export default function Btn({ href, label }: { href: string; label: string }) {
+export default function Btn({
+  type,
+  href,
+  label,
+  disabled,
+  onClick,
+}: {
+  type: 'button' | 'submit' | 'reset';
+  href: string;
+  label: string;
+  disabled: boolean;
+  onClick?: () => void;
+}) {
   return (
-    <Link href={href}>
-      <button>{label}</button>
+    <Link id="button" href={href}>
+      <button type={type} onClick={onClick} disabled={disabled}>
+        {label}
+      </button>
     </Link>
   );
 }
