f91537a39f9c0b8bd957c2679ed2f48dc5499ef6
sunpaaaa <sunpaaaa@gmail.com>
2023-10-12 12:20:14 +0900

    Feat : 메인페이지 헤더 연결

--- src/templates/main/mainPage.tsx
+++ src/templates/main/mainPage.tsx
@@ -2,13 +2,38 @@ import { getProducts } from '@/api/product';
 import Header from '@/components/header';
 import ProductList from '@/components/productList';
 import { IProduct } from '@/types/interface';
+import '@/styles/templates/main/main.scss';
+import AddBtn from './addBtn';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import Link from 'next/link';
 
 export default async function MainPage() {
   const data: IProduct[] = await getProducts();
   return (
-    <>
-      <Header title={'개발자님'} border={true} />
-      <ProductList data={data} />
-    </>
+    <div id="mainPage">
+      <header>
+        <Header
+          title={'개발자님'}
+          border={true}
+          button={
+            <>
+              <Link href="search">
+                <AiOutlineSearch className="header__btn" />
+              </Link>
+              <Link href="category">
+                <RxHamburgerMenu className="header__btn" />
+              </Link>
+            </>
+          }
+        />
+      </header>
+      <section className="main__content">
+        <ProductList data={data} />
+        <AddBtn />
+      </section>
+
+      <nav>navbar</nav>
+    </div>
   );
 }
