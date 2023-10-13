9fafa73fcd291a27fee0eef7bb4bb55871994bb4
Hojin <wkdghwls0623@gmail.com>
2023-10-13 18:06:44 +0900

    Fix:axios->fetch로 변경

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,12 +1,41 @@
-import axios from 'axios';
+// const client = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+// });
 
-const client = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-});
+// export const getProducts = async () => {
+//   const res = await client.get('/products');
+//   return res.data;
+// };
+
+// export const postSignUp = async (
+//   email: string,
+//   password: string,
+//   phone: string,
+//   nickname: string,
+// ) => {
+//   const res = await client.post('/signup', {
+//     email: email,
+//     password: password,
+//     phone: phone,
+//     nickname: nickname,
+//   });
+//   return res;
+// };
+
+// export const getAuth = async (email: string, password: string) => {
+//   const res = await client.post('/login', {
+//     email: email,
+//     password: password,
+//   });
+//   return res;
+// };
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const getProducts = async () => {
-  const res = await client.get('/products');
-  return res.data;
+  const res = await fetch(`${baseURL}/products`);
+  const data = await res.json();
+  return data;
 };
 
 export const postSignUp = async (
@@ -15,19 +44,31 @@ export const postSignUp = async (
   phone: string,
   nickname: string,
 ) => {
-  const res = await client.post('/signup', {
-    email: email,
-    password: password,
-    phone: phone,
-    nickname: nickname,
+  const res = await fetch(`${baseURL}/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+      phone,
+      nickname,
+    }),
   });
   return res;
 };
 
 export const getAuth = async (email: string, password: string) => {
-  const res = await client.post('/login', {
-    email: email,
-    password: password,
+  const res = await fetch(`${baseURL}/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
   });
   return res;
 };
