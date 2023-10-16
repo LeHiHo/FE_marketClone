579f0e262b44d21c9c17003324603b3258390d62
kyu <qorudrb14@naver.com>
2023-10-16 14:48:15 +0900

    컨플릭트 해결

--- src/templates/mypage/mypageSale.tsx
+++ src/templates/mypage/mypageSale.tsx
@@ -1,23 +1,43 @@
+'use client';
+import 'src/styles/templates/mypage/mypageSales.scss'
+import { useEffect, useState, Fragment } from 'react';
 import { getProducts } from '@/api/service';
 import Header from '@/components/header';
 import ProductItem from '@/components/productItem';
 import ProductState from '@/components/productState';
-import { AXIOSResponse, IProduct } from '@/types/interface';
 import ProductStateList from '@/templates/product/productStateList/productStateList';
+import { AXIOSResponse, IProduct } from '@/types/interface';
 
-export default async function MypageSale() {
-  const res: AXIOSResponse<IProduct[]> = await getProducts();
-  const data = res.statusCode === 200 ? res.data : [];
+export default function MypageSale() {
+  const [data, setData] = useState<IProduct[]>([]);
+  const [hi, setHi] = useState('all');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res: AXIOSResponse<IProduct[]> = await getProducts();
+      if (res.statusCode === 200) {
+        setData(res.data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const test = data.filter((product) => product.status === '판매중');
+  console.log('all');
+
   return (
     <>
       <Header goBack={true} border={true} title={'판매내역'} />
       <ProductStateList />
-      {data.map((product) => (
-        <>
-          <ProductItem key={product.id} product={product} />
+      <ul className='product-list'>
+      {test.map((product) => (
+        <Fragment key={product.id}>
+          <ProductItem product={product} />
           <ProductState />
-        </>
+        </Fragment>
       ))}
+      </ul>
     </>
   );
 }
--- src/templates/product/productStateList/productStateList.tsx
+++ src/templates/product/productStateList/productStateList.tsx
@@ -3,11 +3,14 @@ import 'src/styles/templates/product/productStateList.scss';
 export default function ProductStateList() {
   return (
     <>
-      <div className='productStateList'>
-        <div className='productState active'>
+      <div className="productStateList">
+        <div className="productState active">
+          <p>전체</p>
+        </div>
+        <div className="productState">
           <p>판매중</p>
         </div>
-        <div className='productState'>
+        <div className="productState">
           <p>거래완료</p>
         </div>
       </div>
