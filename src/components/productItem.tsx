8c7efbb65b7a5eed1fb84f833f3cb1ca20022192
kyu <qorudrb14@naver.com>
2023-10-20 15:18:35 +0900

    Feat:상품 상태에 따라 뱃지 추가

--- src/components/productItem.tsx
+++ src/components/productItem.tsx
@@ -16,6 +16,12 @@ export default function ProductItem({ product }: { product: IProduct }) {
       <div className="product__content">
         <div className="title">{product.title}</div>
         <div className="price">{product.price}원</div>
+        {product.status === '예약중' && (
+          <div className="badge reserved">{product.status}</div>
+        )}
+        {product.status === '거래완료' && (
+          <div className="badge sold">{product.status}</div>
+        )}
         <div className="like">
           {product.like ? <AiFillHeart /> : <AiOutlineHeart />}
           <p className="like__counter">
