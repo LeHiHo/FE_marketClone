fae896c9ee59a274b97cb3f3e325b9e0fa32ab66
sunpaaaa <sunpaaaa@gmail.com>
2023-10-21 12:09:20 +0900

    Docs : utils 폴더 생성

---
+++ src/utils/client.ts
@@ -0,0 +1,61 @@
+import StompJs from '@stomp/stompjs';
+
+const stompClient = new StompJs.Client({
+  brokerURL: process.env.NEXT_PUBLIC_BROKER_URL,
+});
+
+// stompClient.onConnect = (frame) => {
+//   setConnected(true);
+//   console.log('Connected: ' + frame);
+//   stompClient.subscribe('/topic/greetings', (greeting) => {
+//     showGreeting(JSON.parse(greeting.body).content);
+//   });
+// };
+
+// stompClient.onWebSocketError = (error) => {
+//   console.error('Error with websocket', error);
+// };
+
+// stompClient.onStompError = (frame) => {
+//   console.error('Broker reported error: ' + frame.headers['message']);
+//   console.error('Additional details: ' + frame.body);
+// };
+
+// function setConnected(connected) {
+//   $('#connect').prop('disabled', connected);
+//   $('#disconnect').prop('disabled', !connected);
+//   if (connected) {
+//     $('#conversation').show();
+//   } else {
+//     $('#conversation').hide();
+//   }
+//   $('#greetings').html('');
+// }
+
+// function connect() {
+//   stompClient.activate();
+// }
+
+// function disconnect() {
+//   stompClient.deactivate();
+//   setConnected(false);
+//   console.log('Disconnected');
+// }
+
+// function sendName() {
+//   stompClient.publish({
+//     destination: '/app/hello',
+//     body: JSON.stringify({ name: $('#name').val() }),
+//   });
+// }
+
+// function showGreeting(message) {
+//   $('#greetings').append('<tr><td>' + message + '</td></tr>');
+// }
+
+// $(function () {
+//   $('form').on('submit', (e) => e.preventDefault());
+//   $('#connect').click(() => connect());
+//   $('#disconnect').click(() => disconnect());
+//   $('#send').click(() => sendName());
+// });
