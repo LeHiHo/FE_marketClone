3d3479551e25f90db0f676fadef93d2ceb6ac9fc
Hojin <wkdghwls0623@gmail.com>
2023-11-05 19:48:28 +0900

    Feat: ssr test

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,42 @@
 import MainPage from '@/templates/main/mainPage';
+import { getProducts } from '@/api/service';
 
-export default function Main() {
+// export const getProducts = async (searchWord?: string, category?: string) => {
+//   const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+//   const url = new URL('/products', baseURL);
+//   const params = {
+//     searchWord,
+//     categoryNames: category,
+//     pageSize: 300,
+//   };
+
+//   Object.keys(params).forEach(
+//     (key) => params[key] && url.searchParams.append(key, params[key]),
+//   );
+
+//   try {
+//     const res = await fetch(url.toString(), {
+//       credentials: 'include',
+//     });
+
+//     if (!res.ok) {
+//       const text = await res.text(); // read the body as text
+//       throw new Error(`Fetch failed: ${res.status} - ${text}`);
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('There was a problem with the fetch operation:', error);
+//   }
+// };
+
+export default async function Main() {
+  const data = await getProducts();
+
   return (
     <>
-      <MainPage />
+      <MainPage data={data} />
     </>
   );
 }
