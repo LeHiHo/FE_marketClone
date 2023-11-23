e2e9acfa5a65394e08a7f96b77d959bfdd1230fb
HoJin <134940630+LeHiHo@users.noreply.github.com>
2023-11-23 20:56:18 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import '@/styles/globals.scss';
 export const metadata = {
   title: '오이마켓',
   description: '초기 당근마켓 클론',
-    icons: {
-    icon: "https://symbl.cc/i/webp/6e/7e745aed8655c66ddfa8d62223dd05.webp"
-  }
+  icons: {
+    icon: 'https://symbl.cc/i/webp/6e/7e745aed8655c66ddfa8d62223dd05.webp',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
