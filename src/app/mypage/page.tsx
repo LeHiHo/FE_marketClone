453b0dbd7bce98f00140440643baf9590101de1b
Hojin <wkdghwls0623@gmail.com>
2023-12-22 01:01:02 +0900

    test: 데이터 패칭 테스트

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,8 +1,19 @@
 import MypagePage from '@/templates/mypage/mypagePage';
+import { getMyProductfetch } from '@/api/service';
+import { cookies } from 'next/headers';
 
-export default function Mypage() {
+export default async function Mypage() {
+  const accessToken = cookies().get('access-token');
+  let myProductData = null;
+
+  if (accessToken) {
+    const token = accessToken.value;
+    const res = await getMyProductfetch(token);
+    myProductData = res.data;
+  }
   return (
     <>
+      <div>{myProductData}</div>
       <MypagePage />
     </>
   );
