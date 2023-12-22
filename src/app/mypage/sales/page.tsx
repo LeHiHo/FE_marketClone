6c873bb3f89750cc0da70c9bacb19eed8c0a0df3
Hojin <wkdghwls0623@gmail.com>
2023-12-22 11:07:32 +0900

    Fix: Fetch body오류해셜(두번읽기)

--- src/app/mypage/sales/page.tsx
+++ src/app/mypage/sales/page.tsx
@@ -5,12 +5,9 @@ import { cookies } from 'next/headers';
 export default async function Mypage() {
   const accessToken = cookies().get('access-token');
   let myProductData = null;
-  console.log(accessToken);
-
   if (accessToken) {
     const token = accessToken.value;
     const res = await getMyProductfetch(token);
-    console.log(res);
     myProductData = res.data;
   }
 
