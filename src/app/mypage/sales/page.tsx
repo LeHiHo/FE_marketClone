3422aeabbc2401b6f08f60317ffdf6849090b3e6
Hojin <wkdghwls0623@gmail.com>
2023-12-30 16:59:30 +0900

    Env: test 코드 추가

--- src/app/mypage/sales/page.tsx
+++ src/app/mypage/sales/page.tsx
@@ -4,11 +4,14 @@ import { cookies } from 'next/headers';
 
 export default async function Mypage() {
   const accessToken = cookies().get('access-token');
+  console.log('엑세스토큰', accessToken);
   let myProductData = null;
   if (accessToken) {
     const token = accessToken.value;
     const res = await getMyProductfetch(token);
+    console.log(res);
     myProductData = res.data;
+    console.log(myProductData);
   }
 
   return (
